f28c2707a9e2057631def6e9827424df7bde27ae
TH <aisurdragon@hotmail.com>
2025-01-18 02:23:06 -0500

    accidentally removed the imports

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import pygame
+from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
