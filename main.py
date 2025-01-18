2749b96c05a65d38ac44bee450ed5fa9633a42c9
TH <aisurdragon@hotmail.com>
2025-01-18 01:07:30 -0500

    Generalized the update and draw functions!

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def main():
     dt = 0
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    print(updatable,drawable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+            print("updating",object)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
